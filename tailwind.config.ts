d45f0273ce3833bfecc58ba5d6f670bb56ce717e
Francisco Guerra <paco.gq.0709@gmail.com>
2024-02-15 23:40:32 -0600

    initial layout and design of homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        azulito: {
+          100: '#02bec0',
+        },
+        amarillo: {
+          100: '#ece04c',
+        },
+      }
     },
   },
   plugins: [],
