6165695cd71f51b8d7dbffa0ca1c676047053b50
Francisco Guerra <paco.gq.0709@gmail.com>
2024-08-09 00:26:23 -0600

    fix font bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,7 +80,7 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["Inter var", ...fontFamily.sans],
       },
     },
   },
