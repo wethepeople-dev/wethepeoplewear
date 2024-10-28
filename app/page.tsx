4ab66a1a6013768170cfc1e6fd802a3a7104fb86
Francisco Guerra <paco.gq.0709@gmail.com>
2024-10-28 13:55:59 -0600

    fix ig redirect on load

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import Navbar from "@/components/Navbar";
 import Autoplay from "embla-carousel-autoplay"
 import { type CarouselApi } from "@/components/ui/carousel"
 import { InstagramEmbed } from 'react-social-media-embed';
+import Script from 'next/script';
 
 import {
   Carousel,
@@ -44,19 +45,6 @@ interface Product {
 }
 
 export default function Home() {
-
-  useEffect(() => {
-    // Dynamically load the TikTok embed script
-    const script = document.createElement('script');
-    script.src = "https://www.embedista.com/j/tiktok.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      // Clean up the script when the component unmounts
-      document.body.removeChild(script);
-    };
-  }, []);
 
   function convertToTwoDigits(num: number): string {
     return num.toString().padStart(2, '0');
@@ -477,9 +465,10 @@ export default function Home() {
       </section>
 
       {/* SOCIAL MEDIA FEED */}
-      {/* <div className="relative py-10 pt-14">
+      <div className="relative py-10 pt-14">
         <div className="flex flex-wrap w-full">
 
+          {/* texto */}
           <div className="w-full px-10 md:px-20 mx-auto text-center lg:text-left lg:w-6/12 lg:flex lg:items-center">
             <div className="hero-content">
               <h1 className="mb-6 lg:text-7xl md:text-5xl text-4xl font-bold tracking-tight">
@@ -492,7 +481,151 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="w-full px-4 py-10 lg:w-6/12 mx-auto text-center justify-center flex flex-row gap-4">
+          {/* embed */}
+          <div className="w-full px-4 py-10 lg:w-6/12 mx-auto">
+            <div className="z-10 lg:pt-0 flex justify-center">
+
+              <blockquote
+                className="instagram-media mx-auto"
+                data-instgrm-permalink="https://www.instagram.com/wethepeople.wear/"
+                data-instgrm-version="12"
+                style={{
+                  background: '#FFF',
+                  border: 0,
+                  borderRadius: '3px',
+                  boxShadow: '0 0 1px 0 rgba(0,0,0,0.5), 0 1px 10px 0 rgba(0,0,0,0.15)',
+                  margin: '1px auto',
+                  maxWidth: '540px',
+                  minWidth: '326px',
+                  padding: 0,
+                  width: '99.375%',
+                }}
+              >
+                <div style={{ padding: '16px' }}>
+                  <a
+                    id="main_link"
+                    href="https://www.instagram.com/wethepeople.wear/"
+                    style={{
+                      background: '#FFFFFF',
+                      lineHeight: 0,
+                      padding: '0 0',
+                      textAlign: 'center',
+                      textDecoration: 'none',
+                      width: '100%',
+                    }}
+                    target="_blank"
+                  >
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                      <div
+                        style={{
+                          backgroundColor: '#F4F4F4',
+                          borderRadius: '50%',
+                          flexGrow: 0,
+                          height: '40px',
+                          marginRight: '14px',
+                          width: '40px',
+                        }}
+                      ></div>
+                      <div
+                        style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          flexGrow: 1,
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <div
+                          style={{
+                            backgroundColor: '#F4F4F4',
+                            borderRadius: '4px',
+                            flexGrow: 0,
+                            height: '14px',
+                            marginBottom: '6px',
+                            width: '100px',
+                          }}
+                        ></div>
+                        <div
+                          style={{
+                            backgroundColor: '#F4F4F4',
+                            borderRadius: '4px',
+                            flexGrow: 0,
+                            height: '14px',
+                            width: '60px',
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                    <div style={{ padding: '19% 0' }}></div>
+                    <div style={{ display: 'block', height: '50px', margin: '0 auto 12px', width: '50px' }}>
+                      {/* SVG Icon */}
+                    </div>
+                    <div style={{ paddingTop: '8px' }}>
+                      <div
+                        style={{
+                          color: '#3897f0',
+                          fontFamily: 'Arial,sans-serif',
+                          fontSize: '14px',
+                          fontWeight: 550,
+                          lineHeight: '18px',
+                        }}
+                      >
+                        View this post on Instagram
+                      </div>
+                    </div>
+                    <div style={{ padding: '12.5% 0' }}></div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        marginBottom: '14px',
+                        alignItems: 'center',
+                      }}
+                    >
+                      {/* Additional styling and content */}
+                    </div>
+                  </a>
+                  <p
+                    style={{
+                      color: '#c9c8cd',
+                      fontFamily: 'Arial,sans-serif',
+                      fontSize: '14px',
+                      lineHeight: '17px',
+                      marginBottom: 0,
+                      marginTop: '8px',
+                      overflow: 'hidden',
+                      padding: '8px 0 7px',
+                      textAlign: 'center',
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap',
+                    }}
+                  >
+                    <a
+                      href="https://www.instagram.com/wethepeople.wear/"
+                      style={{
+                        color: '#c9c8cd',
+                        fontFamily: 'Arial,sans-serif',
+                        fontSize: '14px',
+                        fontStyle: 'normal',
+                        fontWeight: 'normal',
+                        lineHeight: '17px',
+                        textDecoration: 'none',
+                      }}
+                      target="_blank"
+                    >
+                      Shared post
+                    </a>{' '}
+                    on <time style={{ fontFamily: 'Arial,sans-serif', fontSize: '14px', lineHeight: '17px' }}>Time</time>
+                  </p>
+                </div>
+              </blockquote>
+              <Script src="https://www.instagram.com/embed.js" strategy="lazyOnload" />
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="w-full px-4 py-10 lg:w-6/12 mx-auto text-center justify-center flex flex-row gap-4">
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
               <InstagramEmbed url="https://www.instagram.com/p/CxbQP5ZJciG/" width={328} />
@@ -504,11 +637,7 @@ export default function Home() {
               </div>
             </div>
 
-          </div>
-
-
-        </div>
-      </div> */}
+          </div> */}
 
       {/* FOOTER */}
       <Footer />
