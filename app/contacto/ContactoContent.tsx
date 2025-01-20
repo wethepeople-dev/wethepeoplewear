b6faeb044c11a185e30d4400636b25637b9aade5
Francisco Guerra <paco.gq.0709@gmail.com>
2025-01-20 16:15:38 -0600

    remove collectif

--- app/contacto/ContactoContent.tsx
+++ app/contacto/ContactoContent.tsx
@@ -51,7 +51,7 @@ export default function ContactoContent() {
                 <div className="mx-auto pb-8">
 
                     {/* collectif */}
-                    <section className="relative overflow-hidden bg-azulito-100 text-white">
+                    {/* <section className="relative overflow-hidden bg-azulito-100 text-white">
                         <div className="container mx-auto px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
                             <div className="grid gap-8 lg:grid-cols-2 lg:gap-16">
                                 <div className="flex flex-col justify-center">
@@ -92,7 +92,24 @@ export default function ContactoContent() {
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </section> */}
+
+                    {/* header contacto */}
+                    <div className="isolate px-6 py-16 lg:px-8 bg-azulito-100">
+                        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
+                            <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-azulito-100 to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                                style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}
+                            ></div>
+                        </div>
+                        <div className="mx-auto max-w-2xl text-center">
+                            <h2 className="text-balance text-4xl font-extrabold tracking-tight text-gray-100 sm:text-5xl">
+                                Contáctanos
+                            </h2>
+                            <p className="mt-2 text-lg font-normal text-gray-200">
+                                ¿Tienes alguna duda, sugerencia o comentario? Escríbenos y con gusto te responderemos.
+                            </p>
+                        </div>
+                    </div>
 
                     {/* redes sociales y forms */}
                     {/* https://readymadeui.com/tailwind-blocks/contact-form */}
