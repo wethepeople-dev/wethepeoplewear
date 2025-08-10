326f215db3f03fcfc47393179a2b75063137bd54
Francisco Guerra <paco.gq.0709@gmail.com>
2025-08-10 14:20:24 -0600

    add pixel code meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { CartProvider } from "@/lib/CartContext";
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 
 import { cn } from "@/lib/utils"
 
@@ -13,7 +15,6 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   metadataBase: process.env.NEXT_PUBLIC_HOST ? new URL(process.env.NEXT_PUBLIC_HOST) : undefined,
-  // title: "We The People Wear | T-Shirts con diseños positivos",
   title: {
     default: "We The People Wear | T-Shirts con diseños positivos",
     template: "%s | We The People Wear",
@@ -36,6 +37,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '4150167215271872');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Meta Pixel NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=4150167215271872&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
+
       <CartProvider>
         <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
           {children}
